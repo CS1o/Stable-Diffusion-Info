ff3af13947d9e9f8930c2aa7936c2c934ea643fe
CS1o <54905616+CS1o@users.noreply.github.com>
2026-01-30 21:59:48 +0100

    Added ComfyUI-Start.bat

---
+++ Resources/ComfyUI-Start.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Launching ComfyUI...
+echo.
+
+call venv\Scripts\activate
+
+REM Speed improvement flags
+set FLASH_ATTENTION_TRITON_AMD_ENABLE=TRUE
+set TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1
+
+REM Launch args
+python main.py --auto-launch --use-pytorch-cross-attention --disable-smart-memory --bf16-vae
